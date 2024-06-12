911166a2c6d60a2ffb3a462fc0d6181369b66321
Timofii Shkabrov <timofii.shkabrov@gmail.com>
2024-06-12 11:00:14 +0200

    Initial Commit

---
+++ LED equivalent/LED_equivalentApp.swift
@@ -0,0 +1,17 @@
+//
+//  LED_equivalentApp.swift
+//  LED equivalent
+//
+//  Created by Тимофей Шкабров on 12/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct LED_equivalentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
