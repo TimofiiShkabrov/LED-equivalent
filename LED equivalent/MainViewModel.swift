af0f627433f9a81c7bd796b2a6a75a0f07a460a3
Timofii Shkabrov <timofii.shkabrov@gmail.com>
2024-06-12 18:53:41 +0200

    Version: 1.0 - An application with basic functionality has been created

---
+++ LED equivalent/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  LED equivalent
+//
+//  Created by Тимофей Шкабров on 12/06/2024.
+//
+
+import Foundation
---
+++ LED equivalent/View/OtherView/ParametersView.swift
@@ -0,0 +1,18 @@
+//
+//  ParametersView.swift
+//  LED equivalent
+//
+//  Created by Тимофей Шкабров on 12/06/2024.
+//
+
+import SwiftUI
+
+struct ParametersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParametersView()
+}
